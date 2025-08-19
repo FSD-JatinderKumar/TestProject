4df96004805f661fa2f885c578a814aae7c33025
Jatindar Kumaar SA <developer.jatinderk@gmail.com>
2025-08-19 16:40:36 +0530

    Angular 19 wokring

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ClickToCallComponent } from './click-to-call/click-to-call.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:"",
+        component:ClickToCallComponent
+
+    },
+];
